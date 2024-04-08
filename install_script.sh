e114de8dafdfd18d7610fb7037a6b9277d4b8a7e
Yapon <slipnot174@gmail.com>
2024-04-08 19:08:56 +0300

    install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# Клонирование репозитория
-cd /home/user/ && git clone https://github.com/ChipiCoin/ChipiCoinGpuMiner.git
+# Установка git и переход в директорию пользователя
+apt install git -y && cd /home/user/ && 
 
-# Установка Node Version Manager (NVM)
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.bashrc
-nvm install 16
+# Клонирование репозитория ChipiCoinGpuMiner
+git clone https://github.com/ChipiCoin/ChipiCoinGpuMiner.git && cd ChipiCoinGpuMiner && 
 
-# Загрузка и распаковка POW Miner
-wget https://github.com/tontechio/pow-miner-gpu/releases/download/20211230.1/minertools-cuda-ubuntu-18.04-x86-64.tar.gz
-tar -xzf minertools-cuda-ubuntu-18.04-x86-64.tar.gz
+# Установка curl
+apt install curl -y && 
 
-# Скачивание и установка скриптов
-wget -O /home/user/run_chipi.sh https://raw.githubusercontent.com/Yapon/test/master/run_chipi.sh
-wget -O /home/user/auto_chipi.sh https://raw.githubusercontent.com/Yapon/test/master/auto_chipi.sh
-chmod +x /home/user/run_chipi.sh
-chmod +x /home/user/auto_chipi.sh
+# Установка nvm и Node.js версии 16
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash && source ~/.bashrc && nvm install 16 && 
 
-# Создание пустого файла config.txt
-touch /home/user/config.txt
+# Скачивание и распаковка minertools
+wget https://github.com/tontechio/pow-miner-gpu/releases/download/20211230.1/minertools-cuda-ubuntu-18.04-x86-64.tar.gz && tar -xzf minertools-cuda-ubuntu-18.04-x86-64.tar.gz && 
+
+# Скачивание необходимых скриптов
+wget -O /home/user/1.sh https://raw.githubusercontent.com/Yapon/test/master/123.sh &&
+wget -O /home/user/run_chipi.sh https://raw.githubusercontent.com/Yapon/test/master/run_chipi.sh && 
+wget -O /home/user/auto_chipi.sh https://raw.githubusercontent.com/Yapon/test/master/auto_chipi.sh && 
+
+# Настройка прав доступа для скриптов
+cd /home/user/ && chmod +x run_chipi.sh &&
+cd /home/user/ && chmod +x 1.sh &&
+cd /home/user/ && chmod +x auto_chipi.sh

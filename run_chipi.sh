f8d7b94a16920c8971fda95affe0877165772462
Yapon <slipnot174@gmail.com>
2024-04-09 23:55:02 +0300

    run_chipi.sh

--- run_chipi.sh
+++ run_chipi.sh
@@ -3,7 +3,4 @@
 screen -ls | grep chipi | cut -d. -f1 | awk '{print $1}' | xargs kill
 # start chipi
 cd /home/user/ChipiCoinGpuMiner
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.bashrc
-nvm install 16
 cd /home/user/ChipiCoinGpuMiner && screen -dmS chipi ./start_multi_8.sh

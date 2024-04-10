f12713925fa94f537cf1afd4cb571c80c27419f4
Yapon <slipnot174@gmail.com>
2024-04-10 20:35:57 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
-
-# Stop all processes in screen with names "chipi" and "auto_miner"
-screen -ls | grep -E '\<chipi\>|\<auto_miner\>' | awk -F '[.]' '/^[[:space:]]*[[:digit:]]/{print $1}' | xargs -I {} screen -S {} -X quit
-# Wait for 3 seconds
-sleep 3
-# Display message: Stopping all processes in screen with names "chipi" and "auto_miner"
-echo "Stopping all processes in screen with names 'chipi' and 'auto_miner'"
-
-# Change directory to user's home directory
-cd /home/user/
-# Wait for 3 seconds
-sleep 3
-# Display message: Changing directory to user's home directory
-echo "Changing directory to user's home directory"
-
-# Start the auto_chipi.sh script in a new screen session named "auto_miner"
-screen -dmS auto_miner ./auto_chipi.sh
-# Wait for 3 seconds
-sleep 3
-# Display message: Starting auto_chipi.sh script in a new screen session named "auto_miner"
-echo "Starting auto_chipi.sh script in a new screen session named 'auto_miner'"
+sleep 1
+# Остановка всех процессов в screen с названием "chipi" и "auto_miner"
+screen -ls | grep -E '\<chipi\>|\<auto_miner\>' | cut -d. -f1 | awk '{print $1}' | xargs -I {} screen -X -S {} quit
+sleep 1
+# Переход в директорию пользователя и запуск скрипта auto_chipi.sh в новом screen-сеансе с названием "auto_miner"
+cd /home/user/ && screen -dmS auto_miner ./auto_chipi.sh

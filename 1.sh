6c97340ac22d80b38b269a7aaabedb414aff2c0f
Yapon <slipnot174@gmail.com>
2024-04-13 17:17:21 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+
+# Function to stop all processes in screen sessions with names "chipi" and "auto_miner"
+stop_screen_processes() {
+    screen -ls | grep -E '\<chipi\>|\<auto_miner\>' | while read -r session_info; do
+        session_name=$(echo "$session_info" | awk '{print $1}')
+        session_status=$(echo "$session_info" | awk '{print $3}')
+        
+        if [[ $session_status == "(Dead" ]]; then
+            screen -S "$session_name" -X quit
+        else
+            screen -S "$session_name" -X stuff "^C"
+            sleep 5
+            if screen -list | grep "$session_name" >/dev/null; then
+                screen -S "$session_name" -X kill
+            fi
+        fi
+    done
+}
+
+# Stop all processes in screen sessions
+stop_screen_processes
+
+# Wait for a brief moment
 sleep 1
-# Остановка всех процессов в screen с названием "chipi" и "auto_miner"
-screen -ls | grep -E '\<chipi\>|\<auto_miner\>' | cut -d. -f1 | awk '{print $1}' | xargs -I {} screen -X -S {} quit
-sleep 1
-# Переход в директорию пользователя и запуск скрипта auto_chipi.sh в новом screen-сеансе с названием "auto_miner"
+
+# Change to the user directory and start the auto_chipi.sh script in a new screen session named "auto_miner"
 cd /home/user/ && screen -dmS auto_miner ./auto_chipi.sh

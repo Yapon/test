44b5258359c3d62610c4cb357dc04b2520e2a6ed
Yapon <slipnot174@gmail.com>
2024-04-13 17:13:22 +0300

    Update run_chipi.sh

--- run_chipi.sh
+++ run_chipi.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+sleep 1
+cd /home/user/ChipiCoinGpuMiner
+# Wait for 1 seconds
+sleep 2
+# Display message: Entering ChipiCoinGpuMiner directory
+echo "Entering ChipiCoinGpuMiner directory"
 # Install Node.js version 16
 nvm install 16
 # Wait for 5 seconds
@@ -6,11 +13,6 @@ sleep 5
 # Display message: Installing Node.js version 16
 echo "Installing Node.js version 16"
 # Start Chipi
-cd /home/user/ChipiCoinGpuMiner
-# Wait for 5 seconds
-sleep 5
-# Display message: Entering ChipiCoinGpuMiner directory
-echo "Entering ChipiCoinGpuMiner directory"
 screen -dmS chipi ./start_multi_8.sh
 # Wait for 5 seconds
 sleep 5

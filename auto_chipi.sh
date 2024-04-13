36591984f7f2506d829475ba9b7327706520d5f1
Yapon <slipnot174@gmail.com>
2024-04-13 17:06:45 +0300

    Update auto_chipi.sh

--- auto_chipi.sh
+++ auto_chipi.sh
@@ -7,23 +7,36 @@ get_total_power() {
 
 # Threshold value for power consumption (in Watts) at which to execute the command
 threshold=65
+# Threshold for the duration of low power consumption (in seconds) to execute the command
+duration_threshold=$((5 * 60))
+
+# Variable to track the duration of low power consumption
+low_power_duration=0
 
 # Infinite loop for polling every 5 minutes
 while true; do
     # Check the current total power consumption of GPUs
     total_power=$(get_total_power)
 
-    # If the consumption is below the threshold, execute the command
+    # If the consumption is below the threshold, increment the low power duration counter
     if [ $(echo "$total_power < $threshold" | bc) -eq 1 ]; then
-        echo "Total power consumption is below $threshold Watts. Executing the command."
+        low_power_duration=$((low_power_duration + 300))
+    else
+        # If consumption is above threshold, reset the low power duration counter
+        low_power_duration=0
+    fi
+
+    # If low power duration exceeds the threshold, execute the command
+    if [ $low_power_duration -ge $duration_threshold ]; then
+        echo "Total power consumption has been below $threshold Watts for more than 5 minutes. Executing the command."
         cd /home/user
         sleep 1
         ./run_chipi.sh
         sleep 1
-    else
-        echo "Total power consumption is $total_power Watts, which is greater than or equal to $threshold Watts."
+        # Reset the low power duration counter after executing the command
+        low_power_duration=0
     fi
 
-    # Wait for 5 minutes before the next iteration
-    sleep 300
+    # Wait for 2 minutes before the next iteration
+    sleep 120
 done
